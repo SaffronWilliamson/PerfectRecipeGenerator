bef3307b13497de5a2e8002f2b135d6f7661dfc3
swilliamson <saffron.williamson@booking.com>
2025-10-22 13:19:59 +0100

    Added greeting page and content inc user inputs

--- src/GreetingPage.java
+++ src/GreetingPage.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class GreetingPage {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //import scanner
+        Scanner scanner = new Scanner(System.in);
+        //welcome message
+        System.out.println("Welcome to the Perfect Recipe Generator!");
+        //first name
+        System.out.print("Before we start, please enter your first name: ");
+        String firstName = scanner.nextLine();
+        //ask user if they want to continue with the programme
+        System.out.println("Thank you " + firstName + ", are you ready to find your perfect recipe?");
+        System.out.print("Please enter yes or no: ");
+        //provide a message if they want to continue or close the programme if they don't
+        String answer;
+        do {
+            answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("yes")) {
+                System.out.println("Great! Let's get started.");
+                break;
+            } else if (answer.equalsIgnoreCase("no")) {
+                System.out.println("Okay, maybe next time. Goodbye!");
+                return;
+            } else {
+                System.out.println("Invalid input. Please enter yes or no.");
+            }
+        } while (true);
     }
 }
