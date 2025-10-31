a14b02de13702adee29b3b7a0adf602aef376c43
swilliamson <saffron.williamson@booking.com>
2025-10-31 13:20:42 +0000

    added recipe info to my recipe class, added a method to display ingredients

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,3 +1,29 @@
+import java.util.Arrays;
+
 public class Recipe {
     //store recipe info
+
+    //info for each recipe - attributes
+    String recipeName;
+    String[] ingredients;
+    boolean isVegetarian;
+    boolean isSpicy;
+    boolean isLowCalorie;
+
+    public Recipe(String recipeName, String[] ingredients, boolean isVegetarian, boolean isSpicy, boolean isLowCalore) {
+        this.recipeName = recipeName;
+        this.ingredients = ingredients;
+        this.isVegetarian = isVegetarian;
+        this.isSpicy = isSpicy;
+        this.isLowCalorie = isLowCalore;
+    }
+
+    //display recipe details - method
+    public void displayRecipe() {
+        System.out.println("Your perfect recipe is: " + recipeName);
+        System.out.println("Ingredients: " + Arrays.toString(ingredients));
+        for (String ingredient : ingredients) {
+            System.out.println("•" + ingredient);
+        }
+    }
 }
